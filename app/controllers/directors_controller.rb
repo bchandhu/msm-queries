504e802f829c557748a8fb92ad24f3df11b3f4c1
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-03-16 22:48:36 +0000

    Added directors#youngest and directors#eldest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,14 +4,31 @@ class DirectorsController < ApplicationController
       render({:template => "director_templates/list"})
     end
 
+    def youngest
+      @latest_dob = Director.maximum(:dob)
+      
+      @youngest_director = Director.where({ :dob => @latest_dob }).first
+  
+      render({ :template => "director_templates/youngest" })
+    end
+  
+    def eldest
+      @earliest_dob = Director.minimum(:dob)
+      
+      @eldest_director = Director.where({ :dob => @earliest_dob }).first
+  
+      render({ :template => "director_templates/eldest" })
+    end
+
     def show
       the_id = params.fetch("the_id")
-      
-      matching_records = Director.where({:id => the_id})
-
+  
+      matching_records = Director.where({:id=>the_id})
+  
       @the_director = matching_records.at(0)
-
+  
       render({:template => "director_templates/details"})
-    end
+  
+    end 
 
   end
